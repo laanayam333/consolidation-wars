a842c4e462e5ba6a2b5feb92d3268cb5f5e178bd
LUIS ANAYA <brothersailorwebdev@gmail.com>
2022-02-15 00:16:06 +0100

    fixed some lint and husky stuff

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,14 @@
 module.exports = {
-  // Run ESLint on changes to JavaScript/TypeScript files
-  '**/*.(ts|js)?(x)': (filenames) => `yarn lint . ${filenames.join(' ')}`,
+  // Type check TypeScript files
+  '**/*.(ts|tsx)': () => 'yarn tsc --noEmit',
+
+  // Lint then format TypeScript and JavaScript files
+  '**/*.(ts|tsx|js)': (filenames) => [
+    `yarn eslint --fix ${filenames.join(' ')}`,
+    `yarn prettier --write ${filenames.join(' ')}`,
+  ],
+
+  // Format MarkDown and JSON
+  '**/*.(md|json)': (filenames) =>
+    `yarn prettier --write ${filenames.join(' ')}`,
 };
